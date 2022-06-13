41c2c68666f7086244fb642c99c3d76946c38958
Erik Lopes <erik.lopesdev@outlook.com>
2022-06-13 17:02:36 -0300

    criando models

--- app/backend/src/api/App.ts
+++ app/backend/src/api/App.ts
@@ -20,7 +20,7 @@ class App {
   }
 
   private headers(): void {
-    this.app.use((req, res, next) => {
+    this.app.use((_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
