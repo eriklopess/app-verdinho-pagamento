0fe85a28c44618844dec7424956a36fe12d173cf
Erik Lopes <erik.lopesdev@outlook.com>
2022-06-13 16:00:58 -0300

    instalando lint

--- app/backend/api/App.ts
+++ app/backend/api/App.ts
@@ -1,42 +1,42 @@
 import express from 'express';
+import routes from '../routes';
 
 class App {
-    public app: express.Application;
-    constructor() {
-        this.app = express();
-        this.config();
-    }
+  public app: express.Application;
 
-    private middleware(): void {
-        this.app.use(express.json());
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+  }
 
-    private config(): void {
-        this.middleware();
-        this.headers();
-        this.router();
-    }
+  private middleware(): void {
+    this.app.use(express.json());
+  }
 
-    private headers(): void {
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            next();
-        });
-    }
+  private config(): void {
+    this.middleware();
+    this.headers();
+    this.router();
+  }
 
-    private router(): void {
-        this.app.get('/', (req, res) => {
-            res.send('Hello World!');
-        });
-    }
+  private headers(): void {
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      next();
+    });
+  }
 
-    public start(PORT: string | number): void {
-        this.app.listen(PORT, () => {
-            console.log(`Server on port ${PORT}`);
-        });
-    }
+  private router(): void {
+    this.app.use(routes);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => {
+      console.log(`Server on port ${PORT}`);
+    });
+  }
 }
 
 export default App;
---
+++ app/backend/api/index.ts
@@ -0,0 +1,5 @@
+import App from './App';
+
+const server = new App();
+
+server.start(3001);
